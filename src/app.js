ef796cb6709ecef318edef7f374df2719d20ab7a
Amit Dusane <amit.dusane@synerzip.com>
2021-01-14 17:09:10 +0530

    horeku changes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDir = path.join(__dirname, '../public');
 const viewsDir = path.join(__dirname, '../template/views');
@@ -51,6 +52,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
 })
